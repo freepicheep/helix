03085da5afdeb0b2f4f05229c0c219dad7424918
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2025-08-01 14:08:49 -0400

    some updates

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2660,7 +2660,8 @@ fn global_search(cx: &mut Context) {
         Some((path.as_path().into(), Some((*line_num, *line_num))))
     })
     .with_history_register(Some(reg))
-    .with_dynamic_query(get_files, Some(275));
+    .with_dynamic_query(get_files, Some(275))
+    .with_title("Search".into());
 
     cx.push_layer(Box::new(overlaid(picker)));
 }
@@ -3196,7 +3197,8 @@ fn buffer_picker(cx: &mut Context) {
             (cursor_line, cursor_line)
         });
         Some((meta.id.into(), lines))
-    });
+    })
+    .with_title("Buffers".into());
     cx.push_layer(Box::new(overlaid(picker)));
 }
 
@@ -3287,7 +3289,8 @@ fn jumplist_picker(cx: &mut Context) {
         let doc = &editor.documents.get(&meta.id)?;
         let line = meta.selection.primary().cursor_line(doc.text().slice(..));
         Some((meta.id.into(), Some((line, line))))
-    });
+    })
+    .with_title("Jump List".into());
     cx.push_layer(Box::new(overlaid(picker)));
 }
 
@@ -3369,7 +3372,8 @@ fn changed_file_picker(cx: &mut Context) {
             }
         },
     )
-    .with_preview(|_editor, meta| Some((meta.path().into(), None)));
+    .with_preview(|_editor, meta| Some((meta.path().into(), None)))
+    .with_title("Changed Files".into());
     let injector = picker.injector();
 
     cx.editor
@@ -3461,7 +3465,8 @@ pub fn command_palette(cx: &mut Context) {
                         doc.append_changes_to_history(view);
                     }
                 }
-            });
+            })
+            .with_title("Command Palette".into());
             compositor.push(Box::new(overlaid(picker)));
         },
     ));
