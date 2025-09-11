db8bc013796d1bbfe56b116a7e14ce8c12cbcde1
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2025-09-11 16:17:10 -0400

    patchy: Merge 14118/rainbow-indent-guides

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -937,6 +937,7 @@ pub struct IndentGuidesConfig {
     pub render: bool,
     pub character: char,
     pub skip_levels: u8,
+    pub rainbow: bool,
 }
 
 impl Default for IndentGuidesConfig {
@@ -945,6 +946,7 @@ impl Default for IndentGuidesConfig {
             skip_levels: 0,
             render: false,
             character: '│',
+            rainbow: false,
         }
     }
 }
